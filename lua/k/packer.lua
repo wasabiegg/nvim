e4277cb9b1c076f7039bde40cda8ab0dd9859155
k <k@maya.maya.me>
2023-10-24 16:22:39 +0200

    Feat: add miasa theme

--- lua/k/packer.lua
+++ lua/k/packer.lua
@@ -41,7 +41,8 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},     -- Required
         }
     }
-    use 'sainnhe/gruvbox-material'
+    use {"xero/miasma.nvim"}
+    -- use 'sainnhe/gruvbox-material'
     -- use 'rebelot/kanagawa.nvim'
     -- use 'rktjmp/lush.nvim'
     -- use './green'
