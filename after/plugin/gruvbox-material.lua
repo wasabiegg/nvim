38b3ca6bdb34034ab0e9de59f4bc08648e4d7055
wasabiegg <wasabiegg>
2024-06-02 23:06:27 +0900

    fix: wrong config for background

--- after/plugin/gruvbox-material.lua
+++ after/plugin/gruvbox-material.lua
@@ -10,7 +10,8 @@ set.background = 'light'
 -- Set contrast
 -- This configuration option should be placed before `colorscheme gruvbox-material`.
 -- Available values: 'hard', 'medium'(default), 'soft'
-vim.g.gruvbox_material_contrast = 'hard'
+vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_foreground = 'original'
 
 -- For better performance
 vim.g.gruvbox_material_better_performance = 1
